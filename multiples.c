4ba8c943e94a4e9f05baabb7f18d2fcbf6567c82
manjeetharyani <43161653+manjeetharyani@users.noreply.github.com>
2021-10-03 21:29:28 +0530

    Update multiples.c

--- multiples.c
+++ multiples.c
@@ -7,6 +7,7 @@
 
 int main(){
 	int i;
+	int multiplesof2;
 	int multiplesof3;
 	int multiplesof5;
 	int sum;
@@ -14,7 +15,8 @@ int main(){
 	for(i = 1; i < 10; i++){
 		multiplesof3 = 3 * 3;
 		multiplesof5 = 5 * 5;
-		printf("3: %d  \t 5: %d\n", multiplesof3, multiplesof5);
+		multiplesof2 = 2 * 2;
+		printf("3: %d  \t 5: %d \t 2 :%d\n", multiplesof3, multiplesof5, multiplesof2);
 	}
 
 }
